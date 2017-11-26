382d0fd9139c2185bc720e0d070b57acd56392fb
elad.bahar <elad.bahar@overwolf.com>
2017-11-26 18:33:59 +0200

    make Environment.GetFolderPath safe (wrap in try/catch): can crash due to .Net issue

--- simple-io-plugin/Constants.cs
+++ simple-io-plugin/Constants.cs
@@ -7,57 +7,65 @@ using System.Threading.Tasks;
 namespace overwolf.plugins {
   static class Constants {
     public static string kProgramFiles =
-      Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+      GetFolderPathSafe(Environment.SpecialFolder.ProgramFiles);
 
     public static string kProgramFilesX86 =
-      Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+      GetFolderPathSafe(Environment.SpecialFolder.ProgramFilesX86);
 
     public static string kCommonFiles =
-      Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFiles);
+      GetFolderPathSafe(Environment.SpecialFolder.CommonProgramFiles);
 
     public static string kCommonFilesX86 =
-      Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFilesX86);
+      GetFolderPathSafe(Environment.SpecialFolder.CommonProgramFilesX86);
 
     public static string kCommonAppDataFiles =
-      Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+      GetFolderPathSafe(Environment.SpecialFolder.CommonApplicationData);
 
     public static string kDesktop =
-      Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+      GetFolderPathSafe(Environment.SpecialFolder.Desktop);
 
     public static string kWinDir =
-      Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+      GetFolderPathSafe(Environment.SpecialFolder.Windows);
 
     public static string kSysDir =
-      Environment.GetFolderPath(Environment.SpecialFolder.System);
+      GetFolderPathSafe(Environment.SpecialFolder.System);
 
     public static string kSysDirX86 =
-      Environment.GetFolderPath(Environment.SpecialFolder.SystemX86);
+      GetFolderPathSafe(Environment.SpecialFolder.SystemX86);
 
     public static string kMyDocuments =
-      Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+      GetFolderPathSafe(Environment.SpecialFolder.MyDocuments);
 
     public static string kMyVideos =
-      Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+      GetFolderPathSafe(Environment.SpecialFolder.MyVideos);
 
     public static string kMyPictures =
-      Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+      GetFolderPathSafe(Environment.SpecialFolder.MyPictures);
 
     public static string kMyMusic =
-      Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+      GetFolderPathSafe(Environment.SpecialFolder.MyMusic);
 
     public static string kCommonDocuments =
-      Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments);
+     GetFolderPathSafe(Environment.SpecialFolder.CommonDocuments);
 
     public static string kFavorites =
-      Environment.GetFolderPath(Environment.SpecialFolder.Favorites);
+     GetFolderPathSafe(Environment.SpecialFolder.Favorites);
 
     public static string kFonts =
-      Environment.GetFolderPath(Environment.SpecialFolder.Fonts);
+     GetFolderPathSafe(Environment.SpecialFolder.Fonts);
 
     public static string kStartMenu =
-      Environment.GetFolderPath(Environment.SpecialFolder.StartMenu);
+     GetFolderPathSafe(Environment.SpecialFolder.StartMenu);
 
     public static string kLocalApplicationData =
-      Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+     GetFolderPathSafe(Environment.SpecialFolder.LocalApplicationData);
+
+    private static string GetFolderPathSafe(Environment.SpecialFolder folder) {
+      try {
+        return Environment.GetFolderPath(folder);
+      } catch {
+        return string.Empty;
+      }
+    }
   }
 }
